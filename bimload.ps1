bd566ae641ee76c660bd113a948d658140578532
nanoib <baydakov@nanocad.ru>
2024-09-25 18:43:32 +0300

    изменения в заголовке

--- bimload.ps1
+++ bimload.ps1
@@ -47,41 +47,55 @@ function Get-PcLatestVersion($pcLatestProgram, $productVersionPattern) {
 }
 
 function Get-FtpLatestFile($ftpUrl, $ftpFolder, $username, [SecureString]$password) {
-    try {
-        # Составляем полный путь к файлу на FTP
-        $ftpPath = [Uri]::new([Uri]$ftpUrl, $ftpFolder).AbsoluteUri
-        Write-Host "4 - Подключаемся к FTP: $ftpPath"
-        # Создайте объект FtpWebRequest
-        $ftpRequest = [System.Net.FtpWebRequest]::Create($ftpPath)
-        $ftpRequest.Method = [System.Net.WebRequestMethods+Ftp]::ListDirectory
-        $ftpRequest.Credentials = New-Object System.Net.NetworkCredential($username, $password)
-        # Получите ответ от сервера
-        $ftpResponse = $ftpRequest.GetResponse()
-        $responseStream = $ftpResponse.GetResponseStream()
-        $reader = New-Object System.IO.StreamReader($responseStream)
+    $maxAttempts = 3
+    $attempt = 0
+    
+    while ($attempt -lt $maxAttempts) {
+        try {
+            # Составляем полный путь к файлу на FTP
+            $ftpPath = [Uri]::new([Uri]$ftpUrl, $ftpFolder).AbsoluteUri
+            Write-Host "4 - Подключаемся к FTP: $ftpPath"
+            
+            # Объект FtpWebRequest
+            $ftpRequest = [System.Net.FtpWebRequest]::Create($ftpPath)
+            $ftpRequest.Method = [System.Net.WebRequestMethods+Ftp]::ListDirectory
+            $ftpRequest.Credentials = New-Object System.Net.NetworkCredential($username, $password)
+            
+            # Ответ от сервера
+            $ftpResponse = $ftpRequest.GetResponse()
+            $responseStream = $ftpResponse.GetResponseStream()
+            $reader = New-Object System.IO.StreamReader($responseStream)
 
-        # Чтение списка файлов
-        $filesList = $reader.ReadToEnd().Split("`n") | Where-Object { $_ -ne "" }
-
-        # Закрытие потоков
-        $reader.Close()
-        $responseStream.Close()
-        $ftpResponse.Close()
-
-        # Сортируем файлы по алфавиту и выбираем последний
-        $ftpLatestFile = $filesList | Sort-Object | Select-Object -Last 1
-
-        # Удаляем лишние пробелы и символы новой строки из имени файла
-        $ftpLatestFile = $ftpLatestFile.Trim()
-
-        Write-Host "Найдено файлов и папок на FTP: $($filesList.Count)"
-        Write-Host "Файл с последней сборкой: $ftpLatestFile"
-
-        return $ftpLatestFile
-    } catch {
-        Write-Error "Ошибка при получении списка файлов с FTP: $_"
-        return @()
+            # Чтение списка файлов
+            $filesList = $reader.ReadToEnd().Split("`n") | Where-Object { $_ -ne "" }
+    
+            # Закрытие потоков
+            $reader.Close()
+            $responseStream.Close()
+            $ftpResponse.Close()
+    
+            # Сортируем файлы по алфавиту и выбираем последний
+            $ftpLatestFile = $filesList | Sort-Object | Select-Object -Last 1
+    
+            # Удаляем лишние пробелы и символы новой строки из имени файла
+            $ftpLatestFile = $ftpLatestFile.Trim()
+    
+            Write-Host "Найдено файлов и папок на FTP: $($filesList.Count)"
+            Write-Host "Файл с последней сборкой: $ftpLatestFile"
+            
+            return $ftpLatestFile
+        } 
+        catch {
+            $attempt++
+            Write-Warning "Попытка $attempt из $maxAttempts не удалась. Ошибка: $_"
+            if ($attempt -lt $maxAttempts) {
+                Start-Sleep -Seconds 5  # Ждем 5 секунд перед следующей попыткой
+            }
+        }
     }
+    
+    Write-Error "Не удалось получить список файлов с FTP после $maxAttempts попыток."
+    return $null
 }
 
 function Compare-Versions($pcLatestVersion, $ftpLatestFile, $fileVersionPattern) {
